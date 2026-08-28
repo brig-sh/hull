af2e0e392d0bdd8647671a4832231e20e5e33f92
Anastassios Nanos <ananos@nofire.ai>
2026-08-28 15:51:47 +0000

    chore: Correct the copyright holder in file headers
    51 files carried a Nubificus LTD copyright header, added erroneously --
    the rest of the tree already attributes NOFire AI. Make every header
    read the same: Copyright (c) 2026, NOFire AI.
    
    Comment-only change; no code is touched.
    
    Signed-off-by: Anastassios Nanos <ananos@nofire.ai>

--- pkg/ociclient/bundle.go
+++ pkg/ociclient/bundle.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023-2026, Nubificus LTD
+// Copyright (c) 2026, NOFire AI
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
